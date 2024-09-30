e50ae2438e6d77f479279cbd438c8aed5ede2ec7
jpleonardxyz <169338128+jpleonardxyz@users.noreply.github.com>
2024-09-30 11:03:47 +0200

    Add solution for exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    //split the string into an array of its characters
+    const chars = input.split("");
+    //reverse the array in place
+    const revChars = chars.reverse();
+    //return the output as a string
+    return revChars.join("");
 };
 
 // Do not edit below this line
