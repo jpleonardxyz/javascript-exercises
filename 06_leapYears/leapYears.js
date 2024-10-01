b36af39698f2a29a7cde878a4bcd4f855e5737aa
jpleonardxyz <169338128+jpleonardxyz@users.noreply.github.com>
2024-10-01 09:57:15 +0200

    Add solution for exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,19 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+    //leap year only if centenary year and divisible by 400
+    if ((year%100 === 0) && (year%400 === 0)){
+        return true;
+    }
+
+    //if not centenery check is leap year by dividing by 4
+    if ((year%100 !== 0)&&(year%4===0)){
+        return true;
+    }
+
+    //all other years are not leap years so return false by default
+    //unless other conditions are met
+    return false;
+    
 };
 
 // Do not edit below this line
