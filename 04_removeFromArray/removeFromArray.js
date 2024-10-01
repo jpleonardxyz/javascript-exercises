5494727d141fa0d02b99341734079d0e3a2b1ad5
jpleonardxyz <169338128+jpleonardxyz@users.noreply.github.com>
2024-10-01 09:14:06 +0200

    Add solution for exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+
+    //create empty output array
+    let arrOutput = [];
+
+    //check through every item in the input array
+    //if the item does not match the arguments
+    //then push it into the output array
+    for (let item of array){
+        if (!args.includes(item)){
+            arrOutput.push(item);
+        }
+    }
+
+    //return the output array which is now
+    //filled with what we want
+    return arrOutput;
+      
 };
 
 // Do not edit below this line
